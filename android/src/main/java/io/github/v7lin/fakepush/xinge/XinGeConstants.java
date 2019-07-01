40a205898a7d6d3b1294cffd4b5a4a7141697951
v7lin <v7lin@qq.com>
2019-07-01 21:21:52 +0800

    依赖替换

---
+++ android/src/main/java/io/github/v7lin/fakepush/xinge/XinGeConstants.java
@@ -0,0 +1,17 @@
+package io.github.v7lin.fakepush.xinge;
+
+public final class XinGeConstants {
+    private XinGeConstants() {
+    }
+
+    // 华为
+    public static final String META_KEY_HW_APPID = "com.huawei.hms.client.appid";
+
+    // 小米
+    public static final String META_KEY_XIAOMI_APPID = "com.xiaomi.mipush.sdk.appid";
+    public static final String META_KEY_XIAOMI_APPKEY = "com.xiaomi.mipush.sdk.appkey";
+
+    // 魅族
+    public static final String META_KEY_MEIZU_APPID = "com.meizu.cloud.pushsdk.appid";
+    public static final String META_KEY_MEIZU_APPKEY = "com.meizu.cloud.pushsdk.appkey";
+}
